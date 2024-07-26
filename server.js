083318065417504bedbede1e0d20e95074f3ee27
tahir-mansour <tahirelmag@gmail.com>
2024-07-26 12:04:54 +0300

    changed the ports for development and production environments

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
-'use server'
-const {createServer} = require("node:http")
-const next = require('next')
-const {Server} = require("socket.io")
+const { createServer } = require("node:http");
+const next = require("next");
+const { Server } = require("socket.io");
 
 const dev = process.env.NODE_ENV !== "production";
-const hostname = "localhost";
-const port = 3000;
-// when using middleware `hostname` and `port` must be provided below
+const hostname = dev ? "localhost" : "team-space.vercel.app";
+const port = process.env.PORT || 3000;
+
 const app = next({ dev, hostname, port });
 const handler = app.getRequestHandler();
 
@@ -16,45 +15,44 @@ app.prepare().then(() => {
   const io = new Server(httpServer);
 
   io.on("connection", (socket) => {
-    socket.on('hello' , (value) => {
-      console.log( ' iwasclicked' , value);
-      io.emit('message' , 'hey')
-    })
-    socket.on('Groupmessage' , (value) => {
-      console.log('recieved' , value);
-      io.emit('Groupmessage' , value)
-    })
-    socket.on('createTask' , (value) => {
-      console.log('recieved' , value);
-      io.emit('createTask' , value)
-    })
-    socket.on('updateTask' , (value) => {
-      console.log('recieved' , value);
-      io.emit('updateTask' , value)
-    })
-    socket.on('newNote' , (value) => {
-      console.log('recieved note' , value);
-      io.emit('newNote' , value) 
-    })
-    socket.on('updateNote' , (value) => {
-      console.log('recieved note' , value);
-      io.emit('updateNote' , value) 
-    })
-    socket.on('createIssue' , (value) => {
-      console.log('recieved Issue' , value);
-      io.emit('createIssue' , value) 
-    })
-    socket.on('updateIssue' , (value) => {
-      console.log('recieved Issue' , value);
-      io.emit('updateIssue' , value) 
-    })
-  
-    console.log('Your server is now connected')
+    socket.on("hello", (value) => {
+      console.log("iwasclicked", value);
+      io.emit("message", "hey");
+    });
+    socket.on("Groupmessage", (value) => {
+      console.log("received", value);
+      io.emit("Groupmessage", value);
+    });
+    socket.on("createTask", (value) => {
+      console.log("received", value);
+      io.emit("createTask", value);
+    });
+    socket.on("updateTask", (value) => {
+      console.log("received", value);
+      io.emit("updateTask", value);
+    });
+    socket.on("newNote", (value) => {
+      console.log("received note", value);
+      io.emit("newNote", value);
+    });
+    socket.on("updateNote", (value) => {
+      console.log("received note", value);
+      io.emit("updateNote", value);
+    });
+    socket.on("createIssue", (value) => {
+      console.log("received Issue", value);
+      io.emit("createIssue", value);
+    });
+    socket.on("updateIssue", (value) => {
+      console.log("received Issue", value);
+      io.emit("updateIssue", value);
+    });
+
+    console.log("Your server is now connected");
   });
   io.on("disconnect", (socket) => {
-    console.log('User disconnected')
+    console.log("User disconnected");
   });
-  
 
   httpServer
     .once("error", (err) => {
