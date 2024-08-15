6e086c9d03c29ed9adeb4a83f6bd71332e337fdf
tahir-mansour <tahirelmag@gmail.com>
2024-08-15 23:46:35 +0300

    FEAT : added css file to the singleDocs

--- components/ProjectPageComponents/DocsComponenets/SingleDoc.tsx
+++ components/ProjectPageComponents/DocsComponenets/SingleDoc.tsx
@@ -1,24 +1,30 @@
 'use client'
 import TiptapForDocs from '@/components/TipTapForDocs/tipTapForDocs'
-import { Input } from '@mantine/core'
+import { Button, Input } from '@mantine/core'
 import React from 'react'
+import '../DocsComponenets/singleDoc.css'
 
 const SingleDoc = () => {
     const [content , setContent] = React.useState('')
+    console.log("🚀 ~ SingleDoc ~ content:", content)
+    const [edit , setEdit] = React.useState(false)
     const handleContentChange = (newContent: string) => {
         setContent(newContent)
     }
 
   return (
-    <main className=' flex w-full mt-12'>
-        <section className='border-r-4 p-4 flex flex-col '>
+    <main className=' flex w-full mt-12' >
+        <Button>{edit ? 'Preview' : 'Edit'}</Button>
+        {/* <section className='border-r-4 p-4 flex flex-col '>
             <Input placeholder='enter the title of the script'/>
-        </section>
+        </section> */}
         <section className=' flex flex-grow  p-3'>
            <TiptapForDocs
                 content  = {content}
                 onChange={(newContent: string) => handleContentChange(newContent)}
             />
+            {!edit ?<div dangerouslySetInnerHTML={{ __html : content}}/> : null}
+
         </section>
     </main>
   )
