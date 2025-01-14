319139a5c7bd1857292aa6c58f1fe123204ed59e
Tahir <tahirelmag@gmail.com>
2025-01-14 20:32:47 +0200

    working on creating a proposal

---
+++ components/TeamWorkSpaceComponents/SideBarPages/Proposal.tsx
@@ -0,0 +1,37 @@
+"use client";
+import React, { Dispatch, SetStateAction, useEffect } from "react";
+import { TransitionWrapper } from "../TransitionWrapper";
+import MainPage from "@/components/SprintComponents/MainPage";
+import BackLogProvider from "@/components/Contexts/BackLogContext";
+import SprintProvider from "@/components/Contexts/SprintContext";
+
+const Proposal = ({
+  opened,
+  setOpened,
+}: {
+  opened: boolean;
+  setOpened: Dispatch<SetStateAction<boolean>>;
+}) => {
+  useEffect(() => {
+    setOpened(true);
+    return () => setOpened(false);
+  });
+
+  return (
+    <TransitionWrapper opened={opened}>
+      <section className=" w-full h-screen">
+        <div className="  flex flex-col h-full w-full  gap-2 rounded-xl   items-center p-3">
+          <section className="w-full h-full px-8">
+            <BackLogProvider>
+              <SprintProvider>
+                <div className="w-full h-full"></div>
+              </SprintProvider>
+            </BackLogProvider>
+          </section>
+        </div>
+      </section>
+    </TransitionWrapper>
+  );
+};
+
+export default Proposal;
